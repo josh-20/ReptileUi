1c00dea6f31c2f97278d4b49265804729f843923
Josh Hamby <bubbahamby@gmail.com>
2023-03-13 00:06:03 -0600

    almost working create user

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -1,21 +1,48 @@
+import { useState } from "react"
 
 type user = {
+    id: number,
     firstName: string,
     lastName: string,
     email: string,
-    password: string
 }
 
 export const CreateUser = () => {
-
-
-    async function createUser() {
-        
-
+    const [user, setUser] = useState<user>()
+    const [firstName, setFristName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    async function saveUser() {
+      const res = await fetch("http://localhost:3000/", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password
+        })
+      });
+      return (res)
     }
 
 
     return(
-        <h1>Create User Page</h1>
+      <div>
+        <h2>Create User</h2>
+        <p>FirstName</p>
+        <input type="text" placeholder="Text" onChange={e => setFristName(e.target.value)}/>
+        <p>LastName</p>
+        <input type="text" placeholder="Text" onChange={e => setLastName(e.target.value)}/>
+        <p>Email</p>
+        <input type="text" placeholder="Text" onChange={e => setEmail(e.target.value)}/>
+        <p>Password</p>
+        <input type="password" placeholder="Text" onChange={e => setPassword(e.target.value)}/>
+        <br/>
+        <button onClick={saveUser}>CreateUser</button>
+      </div>
     )
 }
