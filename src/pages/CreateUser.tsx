0b338b5ae8e42e523faaf87b28bd82d860145728
Josh Hamby <bubbahamby@gmail.com>
2023-03-12 23:21:14 -0600

    user

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -1,7 +1,20 @@
 
-
+type user = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
+}
 
 export const CreateUser = () => {
+
+
+    async function createUser() {
+        
+
+    }
+
+
     return(
         <h1>Create User Page</h1>
     )
