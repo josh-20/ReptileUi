67567d5288a21a53185317bb5edea82889c48cbf
Josh Hamby <bubbahamby@gmail.com>
2023-03-17 09:28:29 -0600

    fixed pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
 import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom"
-import { ApiContext } from "./contexts/api"
-import { Api } from './lib/api'
 import { SignIn } from './pages/SignIn'
 import { Reptile } from './pages/Reptile'
 import { User } from './pages/User'
@@ -10,33 +8,20 @@ import { CreateUser } from './pages/CreateUser'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <CreateUser />,
-    children: [
-      {
-        path: '/signin',
-        element: <SignIn />
-      },
-      {
-        path: '/reptile',
-        element: <Reptile />
-      },
-      {
-        path: '/',
-        element: <User />
-      }
-    ]
-  }
-])
+    element: <CreateUser />
+  },
+  {
+    path: '/signin',
+    element: <SignIn />
+  },
+  {
+    path: '/reptile',
+    element: <Reptile />
+  }])
 
 export const App = () => {
-  const [api, setApi] = useState(new Api()) 
   return (
-    <>
-      <ApiContext.Provider value={api}>
-        <RouterProvider router={router}/>
-      </ApiContext.Provider>
-    </>
-      
+    <RouterProvider router={router}/>
   )
 }
 
--- src/hooks/useApi.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ApiContext } from "../contexts/api";
-
-export const useApi = () => {
-    const api = useContext(ApiContext);
-    return api;
-}
