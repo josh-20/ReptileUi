1b095b507a140ca22725a69071cd8162787bff00
Josh Hamby <bubbahamby@gmail.com>
2023-03-17 10:05:19 -0600

    almost working signin

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -3,49 +3,50 @@ import { Link, useNavigate } from "react-router-dom"
 
 
 export const CreateUser = () => {
-    const [firstName, setFristName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    async function saveUser() {
-      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: password
-        })
-      });
-      const resultBody = await res.json() 
-      console.log(resultBody)
-    }
+  const navigate = useNavigate();
+  const [firstName, setFristName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  async function saveUser() {
+    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      })
+    });
+    const resultBody = await res.json();
+    navigate("/signin",{replace: true}); 
+  }
 
 
-    return(
-      <form>
-        <h2>Create User</h2>
-        <label>
-          FirstName
-          <input type="text" value={firstName} placeholder="Text" onChange={e => setFristName(e.target.value)}/>
-        </label>
-        <label>
-          LastName
-          <input type="text" value={lastName} placeholder="Text" onChange={e => setLastName(e.target.value)}/>
-        </label>
-        <label>
-          Email
-          <input type="text" value={email} placeholder="Text" onChange={e => setEmail(e.target.value)}/>
-        </label>
-        <label>
-          Password
-          <input type="password" value={password} placeholder="Text" onChange={e => setPassword(e.target.value)}/>
-        </label>
-        <br/>
-        <button type="button" onClick={saveUser}>CreateUser</button>
-      </form>
-    )
+  return(
+    <form className="signup-form">
+      <h2>Create User</h2>
+      <label>
+        FirstName
+        <input type="text" value={firstName} placeholder="Text" onChange={e => setFristName(e.target.value)}/>
+      </label>
+      <label>
+        LastName
+        <input type="text" value={lastName} placeholder="Text" onChange={e => setLastName(e.target.value)}/>
+      </label>
+      <label>
+        Email
+        <input type="email" value={email} placeholder="Text" onChange={e => setEmail(e.target.value)}/>
+      </label>
+      <label>
+        Password
+        <input type="password" value={password} placeholder="Text" onChange={e => setPassword(e.target.value)}/>
+      </label>
+      <br/>
+      <button type="button" onClick={saveUser}>CreateUser</button>
+    </form>
+  )
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,34 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export const SignIn = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    async function login () {
+        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/signin`, {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+          });
+    }
     return(
-        <h1>SignIn page</h1>
+        <form className="signin-form">
+            <label>
+                Email
+                <input type="email" onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <label>
+                Password
+                <input type="password" onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="button" onClick={SignIn}>Sign in</button>
+            <button onClick={() => {navigate("/",{replace: true})}}>Create User</button>
+        </form>
     )
-}
+    }
