07840f38a43ffaf316dae34f1af6a8c70e4941f4
ryangoobs24 <ryan24gubler@gmail.com>
2023-03-19 23:01:30 -0600

    started the dashboard page creation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { SignIn } from './pages/SignIn'
 import { Reptile } from './pages/Reptile'
 import { User } from './pages/User'
 import { CreateUser } from './pages/CreateUser'
+import { Dashboard } from './pages/Dashboard'
+import { Schedules } from './pages/Schedules'
+import { HusbandryRecords } from './pages/HusbandryRecords'
 
 const router = createBrowserRouter([
   {
@@ -17,7 +20,20 @@ const router = createBrowserRouter([
   {
     path: '/reptile',
     element: <Reptile />
-  }])
+  },
+  {
+    path: '/Schedules',
+    element: <Schedules />
+  },
+  {
+    path: '/husbandryRecords',
+    element: <HusbandryRecords />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  }
+])
 
 export const App = () => {
   return (
